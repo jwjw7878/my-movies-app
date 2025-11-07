b18f0ed6ee2f2e87cd10e7c3c21f5d0f098e1ff9
jwjw7878 <jwp000708@gmail.com>
2025-11-08 01:11:39 +0900

    add banner

--- src/pages/Homepage/components/Banner.jsx
+++ src/pages/Homepage/components/Banner.jsx
@@ -1,10 +1,43 @@
 import React from "react";
+import "./Banner.style.css";
+import { CircularProgress } from "@mui/material";
+import Alert from "@mui/material/Alert";
 import { usePopularMovies } from "../../../hooks/usePopularMovies";
 
 const Banner = () => {
-  const { data } = usePopularMovies();
+  const { data, isLoading, isError, error } = usePopularMovies();
   console.log(data);
-  return <div>Banner</div>;
+  if (isLoading)
+    return (
+      <CircularProgress
+        size={100}
+        style={{
+          color: "red",
+          position: "absolute",
+          top: "30%",
+          left: "50%",
+          transform: "translate(-30%, -50%)",
+        }}
+      />
+    );
+  if (isError) return <Alert severity="error">{error.message}</Alert>;
+
+  return (
+    <div
+      className="banner"
+      style={{
+        backgroundImage:
+          "url(" +
+          `https://media.themoviedb.org/t/p/w1066_and_h600_bestv2${data?.results[0].poster_path}` +
+          ")",
+      }}
+    >
+      <div className="text-area">
+        <h1>{data?.results[0].original_title}</h1>
+        <p>{data?.results[0].overview}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
